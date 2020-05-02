ac159a937265a831c97003bade97069e3c78cf1d
BABURAJ KALLARAKKAL <raj.anju@gmail.com>
2020-05-02 16:55:00 -0500

    EC2 with NGINX installed- DONE

--- aws/modules/compute_ec2.tf
+++ aws/modules/compute_ec2.tf
@@ -1,19 +1,29 @@
 resource "aws_instance" "example" {
+
   ami                         = "ami-0c322300a1dd5dc79"
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.subnet_web1.id
-  vpc_security_group_ids = [aws_security_group.sg_web.id,aws_security_group.sg_ssh.id]
+  vpc_security_group_ids      = [aws_security_group.sg_web.id,aws_security_group.sg_ssh.id]
   associate_public_ip_address = "true"
-  key_name = "id_rsa_aws.pem"
+  key_name                    = "devops"
 
+  user_data = "${file("./install_nginx.sh")}"
+
+  /*
   user_data = <<-EOF
               #!/bin/bash
-              echo "Testing httpd server" > index.html
-              #nohup busybox httpd -f -p "${var.server_port}" &
+              yum install -y nginx
+              systemctl enable nginx
+              systemctl start nginx
+              echo 'System information ' >> /usr/share/nginx/html/home.html
+              curl -v http://169.254.169.254/latest/meta-data/local-ipv4 >> /usr/share/nginx/html/home.html
+              curl -v http://169.254.169.254/latest/meta-data/public-hostname >> /usr/share/nginx/html/home.html
+              systemctl reload nginx
               EOF
+              */
 
   tags = {
-    Name		= "ec2_web1"
+    Name		    = "ec2_web1"
     environment 	= var.environment,
     managed_by  	= var.managed_by
   }

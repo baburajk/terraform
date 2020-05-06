c085dca15b2a307fd52ce42bf98716ffc271b9f4
BABURAJ KALLARAKKAL <raj.anju@gmail.com>
2020-05-05 22:42:00 -0500

    LB, EC2 with forwarded targets

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # terraform-cloud
 Terraform Cloud - AWS, GCLOUD, OCI, Azure
 
+1. Terrform Modules to create VPC, NACL, SG, LBR, Target Group, Listeners, Subnets. (TAG 3.0)
+
+Tests ran
+
+babu@meghmalhar:~/meghmalhar/gitroot/terraform/aws/modules\> curl -s lbr-1802745961.us-east-1.elb.amazonaws.com/home.html
+System information
+10.0.60.246ec2-54-80-124-186.compute-1.amazonaws.combabu@meghmalhar:~/meghmalhar/gitroot/terraform/aws/modules\>
+
+babu@meghmalhar:~/meghmalhar/gitroot/terraform/aws/modules\> curl -sL -w "%{http_code}\n" lbr-1802745961.us-east-1.elb.amazonaws.com/index.html -o /dev/null
+200
+
+
 ![](http://www.intercloudzone.com/svgs/oci-terraform.svg)
 
 http://www.intercloudzone.com/infrastructure-as-a-code.html
 
 Project Plan: ( Work-in-Progress)
+
+
--- aws/modules/compute_ec2.tf
+++ aws/modules/compute_ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "ec2_web1" {
 
   ami                         = "ami-0c322300a1dd5dc79"
   instance_type               = "t2.micro"
@@ -7,18 +7,11 @@ resource "aws_instance" "example" {
   associate_public_ip_address = "true"
   key_name                    = "devops"
 
-  user_data = "${file("./install_nginx.sh")}"
+  user_data = file("install_nginx.sh")
 
   /*
   user_data = <<-EOF
               #!/bin/bash
-              yum install -y nginx
-              systemctl enable nginx
-              systemctl start nginx
-              echo 'System information ' >> /usr/share/nginx/html/home.html
-              curl -v http://169.254.169.254/latest/meta-data/local-ipv4 >> /usr/share/nginx/html/home.html
-              curl -v http://169.254.169.254/latest/meta-data/public-hostname >> /usr/share/nginx/html/home.html
-              systemctl reload nginx
               EOF
               */
 
@@ -30,3 +23,21 @@ resource "aws_instance" "example" {
 
 }
 
+resource "aws_instance" "ec2_web2" {
+
+  ami                         = "ami-0c322300a1dd5dc79"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.subnet_web2.id
+  vpc_security_group_ids      = [aws_security_group.sg_web.id,aws_security_group.sg_ssh.id]
+  associate_public_ip_address = "true"
+  key_name                    = "devops"
+
+  user_data = file("install_nginx.sh")
+
+  tags = {
+    Name		    = "ec2_web2"
+    environment 	= var.environment,
+    managed_by  	= var.managed_by
+  }
+
+}
